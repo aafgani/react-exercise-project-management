d8e1536518231c6697d10fce519c1e228e673084
aafgani <andrya.afgani@gmail.com>
2025-10-28 17:08:41 +0700

    feat: :sparkles: add new project [wip]

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,65 @@
+import { useState } from "react";
+import NoProjectSelected from "./components/NoProjectSelected";
+import ProjectSidebar from "./components/ProjectSidebar";
+import NewProject from "./components/NewProject";
+
 function App() {
+  const [projectState, setProjectState] = useState({
+    projects: [],
+    selectedProjectId: undefined,
+  });
+
+  const handleStartAddProject = () => {
+    setProjectState((prevState) => {
+      return {
+        ...prevState,
+        selectedProjectId: null,
+      };
+    });
+  };
+
+  const handleCancelAddProject = () => {
+    setProjectState((prevState) => {
+      return {
+        ...prevState,
+        selectedProjectId: undefined,
+      };
+    });
+  };
+
+  const handleOnProjectAdded = (newProject) => {
+    setProjectState((prevState) => {
+      return {
+        ...prevState,
+        selectedProjectId: undefined,
+        projects: [...prevState.projects, newProject],
+      };
+    });
+  };
+
+  console.log(projectState);
+
+  let content;
+  if (projectState.selectedProjectId === null) {
+    content = (
+      <NewProject
+        onAddProject={handleOnProjectAdded}
+        onCancel={handleCancelAddProject}
+      />
+    );
+  } else if (projectState.selectedProjectId === undefined) {
+    content = <NoProjectSelected onAddProject={handleStartAddProject} />;
+  }
+
   return (
-    <>
-      <h1 className="my-8 text-center text-5xl font-bold">Hello World</h1>
-    </>
+    <main className="h-screen my-8 flex gap-8">
+      {/* Full height with vertical margin */}
+      <ProjectSidebar
+        onAddProject={handleStartAddProject}
+        projects={projectState.projects}
+      />
+      {content}
+    </main>
   );
 }
 
